f3ac59a875524dae85d0ea33e66e1c9b933df9fc
Jake Swenson <jswenson@godaddy.com>
2016-04-19 23:15:07 -0700

    Adding gpg keys and build support

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,8 @@
 
 set -x
 
-mvn deploy --settings settings.xml -DskipTests
+if [ -n "$TRAVIS_TAG" ]; then
+    mvn gpg:sign deploy --settings settings.xml -DskipTests -P sign
+elif [ "$TRAVIS_BRANCH" = "master" ]; then
+    mvn deploy --settings settings.xml -DskipTests
+fi
